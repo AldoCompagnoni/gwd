da5d08bdf0ee6ef07af64531424ab004e58d1472
Ollie Spacey <ogspacey16@gmail.com>
2021-09-16 12:25:43 +0100

    Created preparation script for HKK site. Added sections: clear environment , load packages, read in data.

---
+++ R/clean_format_hkk.R
@@ -0,0 +1,19 @@
+# HKK data preparation script
+# Data: Kambach
+
+# Clear environment ------------------------------
+
+rm(list=ls())
+
+# Load packages ----------------------------------
+library(tidyverse)
+# devtools::install_github("idiv-biodiversity/LCVP")
+library(LCVP)
+# devtools::install_github("idiv-biodiversity/lcvplants")
+library(lcvplants)
+library(leaflet)
+
+# Read in and filter data ------------------------
+
+kam_means <- read.table( 'data/Demographies_with_dbh_means_Kambach.txt', header = TRUE )
+kam_medians <- read.table( 'data/Demographies_with_dbh_medians_Kambach.txt', header = TRUE )
