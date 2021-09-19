6423036c27604f98e13dd2ccd06bb9332c70b231
Ollie Spacey <ogspacey16@gmail.com>
2021-09-19 11:38:16 +0100

    Created preparation script for Ituri (Lenda) site. Added sections: clear environment, load packages, read in data. Filtered data by Ituri (Lenda) site. Prepared site table and checked location.

---
+++ R/clean_format_ituri_lenda.R
@@ -0,0 +1,37 @@
+# ituri_lenda data preparation script
+# Data: Kambach
+
+# Clear environment ------------------------------
+
+rm(list=ls())
+
+# Load packages ----------------------------------
+library(tidyverse)
+# devtools::install_github("idiv-biodiversity/LCVP")
+library(LCVP)
+# devtools::install_github("idiv-biodiversity/lcvplants")
+library(lcvplants)
+library(leaflet)
+
+# Read in and filter data ------------------------
+
+kam_means <- read.table( 'data/Demographies_with_dbh_means_Kambach.txt', header = TRUE )
+kam_medians <- read.table( 'data/Demographies_with_dbh_medians_Kambach.txt', header = TRUE )
+ituri_lenda_means <- dplyr::filter( kam_means, site == "ituri_lenda")
+ituri_lenda_medians <- dplyr::filter( kam_medians, site == "ituri_lenda")
+
+# Prepare site table -----------------------------
+
+# Do this by hand
+site_out <- data.frame( Site_name = 'Ituri (Lenda)',
+                        Latitude  = 1.436800000000,
+                        Longitude = 28.582600000000 )
+
+# Check that the location is sensible sense
+leaflet( data = site_out) %>% 
+  addTiles() %>% 
+  addCircleMarkers(~Longitude, ~Latitude)
+
+# Store site information
+write.csv( site_out, 'results/ituri_lenda_site.csv',
+           row.names = F )
