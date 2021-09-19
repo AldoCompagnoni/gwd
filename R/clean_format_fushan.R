b513c75c43693163fe91286f1fbe8034fe9a7fb2
Ollie Spacey <ogspacey16@gmail.com>
2021-09-19 11:13:57 +0100

    Filtered data for fushan site. Prepared site table and checked coordinates.

--- R/clean_format_fushan.R
+++ R/clean_format_fushan.R
@@ -17,3 +17,21 @@ library(leaflet)
 
 kam_means <- read.table( 'data/Demographies_with_dbh_means_Kambach.txt', header = TRUE )
 kam_medians <- read.table( 'data/Demographies_with_dbh_medians_Kambach.txt', header = TRUE )
+fushan_medians <- dplyr::filter( kam_means, site == "fushan")
+fushan_medians <- dplyr::filter( kam_medians, site == "fushan")
+
+# Prepare site table -----------------------------
+
+# Do this by hand
+site_out <- data.frame( Site_name = 'Fushan',
+                        Latitude  = 24.761400000000,
+                        Longitude = 121.555000000000 )
+
+# Check that the location is sensible sense
+leaflet( data = site_out) %>% 
+  addTiles() %>% 
+  addCircleMarkers(~Longitude, ~Latitude)
+
+# Store site information
+write.csv( site_out, 'results/fushan_site.csv',
+           row.names = F )
