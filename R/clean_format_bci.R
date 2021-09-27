1fa4dd350cd408d2a15ba2e1c8da559bf8c3bab3
Ollie Spacey <ogspacey16@gmail.com>
2021-09-27 21:03:39 +0100

    ggplot2 loading function. Taxonomic table objects extended by "_bci" for specificity. Taxonomic functions extended. Demographic table criteria added.

--- R/clean_format_bci.R
+++ R/clean_format_bci.R
@@ -12,6 +12,7 @@ library(LCVP)
 # devtools::install_github("idiv-biodiversity/lcvplants")
 library(lcvplants)
 library(leaflet)
+library(ggplot2)
 
 # Read in and filter data ------------------------
 
@@ -39,16 +40,33 @@ write.csv( site_out, 'results/bci_site.csv',
 # Prepare taxonomic table ------------------------
 
 # Produce the binomial used for checking
-taxa_df         <- dplyr::select( bci_means, latin) %>%
+taxa_df_bci         <- dplyr::select( bci_means, latin) %>%
   rename( Submitted_Name = latin )
 
 # Create function: get "cleaned" names
 get_clean_names <- function( nam, fuzzy = 0 ) lcvp_search( nam, max.distance = fuzzy )
 
 # Clean names from the Leipzig's list of plants
-clean_l         <- lapply( taxa_df$Submitted_Name , get_clean_names )
-clean_df        <- clean_l %>% bind_rows
-clean_df
+clean_l_bci         <- lapply( taxa_df_bci$Submitted_Name , get_clean_names )
+clean_df_bci        <- clean_l_bci %>% bind_rows
+clean_df_bci
 
 # check whether there are issues
-notfound_df     <- clean_df %>% subset( Score != 'matched' )
+notfound_df_bci     <- clean_df_bci %>% subset( PL.comparison != 'identical' )
+
+# Rerun Leipzig list with fuzzy matching
+reclean_l_bci       <- lapply( notfound_df_bci$Submitted_Name, get_clean_names, 5 )
+reclean_df_bci      <- reclean_l_bci %>% bind_rows
+
+# Examine final file
+reclean_df_bci
+
+# Prepare demographic table ----------------------
+
+# Check for missing/NULL/Inf values?
+# Check for sample size 0
+ggplot(data = bci_means, aes(site, growth_layer1_mean)) + geom_boxplot()
+#find way to repeat for all parameters
+
+# [Make boxplots to explore the data]
+
