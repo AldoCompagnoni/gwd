5f82995a51377926ce3da8ac402422b6443c9776
Ollie Spacey <ogspacey16@gmail.com>
2021-10-01 15:01:12 +0100

    Prepared taxonomic table for HKK site, with functions to: separate NAs to store in unresolved table, get clean names against LCVP database, check for mismatches/typos, check for species without matches, store resolved taxa. Need to modify NA-removal function and check through output of cleaning once NAs have been removed.

--- R/clean_format_hkk.R
+++ R/clean_format_hkk.R
@@ -36,3 +36,56 @@ leaflet( data = site_out) %>%
 write.csv( site_out, 'results/hkk_site.csv',
            row.names = F )
 
+# Prepare taxonomic table ------------------------
+
+# Produce the binomial used for checking
+taxa_df         <- dplyr::select( hkk_means, latin) %>%
+  rename( Submitted_Name = latin )
+
+# Separate NAs
+taxa_df_na <- split(taxa_df, is.na(taxa_df$Submitted_Name))$`TRUE`
+taxa_df_na_rm <- split(taxa_df, is.na(taxa_df$Submitted_Name))$`FALSE`
+
+# Create function: get "cleaned" names
+get_clean_names   <- function( nam, fuzzy = 0.1 ) lcvp_search( nam, max.distance = fuzzy )
+
+# Clean names from the Leipzig's list of plants
+clean_l         <- lapply( taxa_df$Submitted_Name , get_clean_names )
+clean_df        <- clean_l %>% 
+  bind_rows %>% 
+  rename( Submitted_Name      = Search,
+          First_matched_Name  = Input.Taxon,
+          LCVP_Accepted_Taxon = Output.Taxon ) %>% 
+  # check for accepted names
+  mutate( mismatch_test = str_detect( First_matched_Name, 
+                                      Submitted_Name ) )
+
+# visual check of mismatches and alternative spellings
+mismatch_df <- clean_df %>% subset( !mismatch_test )
+check_mismatches <- lapply( mismatch_df$Submitted_Name, lcvp_fuzzy_search )
+# 4 plausible typos which remain in clean data frame
+
+# Check species without matches
+no_match_v <- setdiff( taxa_df$Submitted_Name, 
+                       clean_df$Submitted_Name )
+
+# Rerun Leipzig list with fuzzy matching
+reclean_l       <- lapply( no_match_v, lcvp_fuzzy_search )
+reclean_df      <- reclean_l %>% bind_rows
+# visually select species identified with lcvp_fuzzy_search
+# Only genus specified, 2 taxa remain unresolved
+
+# Final taxonomy files 
+taxa_nofuzzy    <- clean_df
+taxa_fuzzy      <- reclean_df
+# Combine 
+taxa_out        <- bind_rows( taxa_nofuzzy, taxa_fuzzy )
+# Do "taxa unresolved" by hand (taxa with no matches found)
+taxa_unresvd    <- data.frame( Submitted_Name = no_match_v,
+                               site           = 'hkk' )
+
+# store resolved AND unresolved taxa
+write.csv( taxa_out, 'results/hkk_taxa.csv',
+           row.names = F )
+write.csv( taxa_unresvd, 'results/hkk_taxa_unresvd.csv',
+           row.names = F )
