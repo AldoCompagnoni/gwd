18453c63edb78be1452f2a7ddd5f0e2a408b7e6d
Ollie Spacey <ogspacey16@gmail.com>
2021-10-07 10:01:11 +0100

    Added function to separate NAs. Found typos and separated unresolved mismatches. Created data frame to store unmatched taxa, re-added 10 "Unidentified ." and 1 "Trichoscypha sp." that were ignored. Removed unmatched taxa. Recreated clean data frame with all search variables and exported clean data frame. Exported unresolved taxa (unresolved mismatches plus unmatched).

--- R/clean_format_korup.R
+++ R/clean_format_korup.R
@@ -42,6 +42,10 @@ write.csv( site_out, 'results/korup_site.csv',
 taxa_df         <- dplyr::select( korup_means, latin) %>%
   rename( Submitted_Name = latin )
 
+# Separate NAs - no NAs
+taxa_na_df <- subset( taxa_df,  is.na( Submitted_Name ) )
+taxa_df       <- subset( taxa_df, !is.na( Submitted_Name ) )
+
 # Create function: get "cleaned" names
 get_clean_names   <- function( nam, fuzzy = 0.1 ) lcvp_search( nam, max.distance = fuzzy )
 
@@ -59,26 +63,39 @@ clean_df        <- clean_l %>%
 # visual check of mismatches and alternative spellings
 mismatch_df <- clean_df %>% subset( !mismatch_test )
 check_mismatches <- lapply( mismatch_df$Submitted_Name, lcvp_fuzzy_search )
-# 4 plausible typos which remain in clean data frame
+# 9 plausible typos which remain in clean data frame
+
+# remove 18 unresolved species containing "sp." from clean data frame
+mismatch_unresvd <- data.frame("Submitted_Name" = grep( 'sp[[:punct:]]', mismatch_df$Submitted_Name, value = T ))
+clean_df_final <- data.frame("Submitted_Name" = grep( 'sp[[:punct:]]', clean_df$Submitted_Name, value = T, invert = T ))
 
 # Check species without matches
-no_match_v <- setdiff( taxa_df$Submitted_Name, 
-                       clean_df$Submitted_Name )
+no_match_v <- data.frame( "Submitted_Name" = setdiff( taxa_df$Submitted_Name, 
+                                                      clean_df$Submitted_Name ))
+# 10 "Unidentified ." and 1 "Trichoscypha sp." ignored (found visually by comparing taxa_df and clean_df); add in manually
+no_match_v[44:53,] <- "Unidentified ."
+no_match_v[54,] <- "Trichoscypha sp."
 
 # Rerun Leipzig list with fuzzy matching
 reclean_l       <- lapply( no_match_v, lcvp_fuzzy_search )
 reclean_df      <- reclean_l %>% bind_rows
 # visually select species identified with lcvp_fuzzy_search
-# Only genus specified, 2 taxa remain unresolved
+# Only genus specified, 43 unmatched taxa remain unresolved
 
 # Final taxonomy files 
-taxa_nofuzzy    <- clean_df
-taxa_fuzzy      <- reclean_df
-# Combine 
-taxa_out        <- bind_rows( taxa_nofuzzy, taxa_fuzzy )
+# Clean taxa should have LCVP search results
+taxa_out        <- lapply( clean_df_final$Submitted_Name, get_clean_names ) %>% 
+  bind_rows %>% 
+  rename( Submitted_Name      = Search,
+          First_matched_Name  = Input.Taxon,
+          LCVP_Accepted_Taxon = Output.Taxon ) %>% 
+  mutate( mismatch_test = str_detect( First_matched_Name, 
+                                      Submitted_Name ) )
+
 # Do "taxa unresolved" by hand (taxa with no matches found)
-taxa_unresvd    <- data.frame( Submitted_Name = no_match_v,
-                               site           = 'korup' )
+taxa_unresvd    <- bind_rows( mismatch_unresvd, no_match_v ) %>%
+  mutate( site = 'korup' )
+
 
 # store resolved AND unresolved taxa
 write.csv( taxa_out, 'results/korup_taxa.csv',
