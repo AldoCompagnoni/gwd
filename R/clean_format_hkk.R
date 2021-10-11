83a4abe4775b39236ded094f9eb954947132a0b6
Ollie Spacey <ogspacey16@gmail.com>
2021-10-11 18:38:08 +0100

    Added Sp_Code, Submitted_Genus, Submitted_Family and IDlevel variables to taxa_df so they are included in unresolved table. Renamed filtered table with and without NAs. Added site column to clean taxa output table. Re-incorporated extra variables (Sp_Code, Submitted_Genus, Submitted_Family and IDlevel) to taxa_unresvd.

--- R/clean_format_hkk.R
+++ R/clean_format_hkk.R
@@ -39,18 +39,18 @@ write.csv( site_out, 'results/hkk_site.csv',
 # Prepare taxonomic table ------------------------
 
 # Produce the binomial used for checking
-taxa_df         <- dplyr::select( hkk_means, latin ) %>%
-  rename( Submitted_Name = latin )
+taxa_df         <- dplyr::select( hkk_means, latin, sp, genus, family, IDlevel ) %>%
+  rename( Submitted_Name = latin, Sp_Code = sp, Submitted_Genus = genus, Submitted_Family = family )
 
 # Separate NAs - genus names only are unresolved
-taxa_genus_df <- subset( taxa_df,  is.na( Submitted_Name ) )
-taxa_df       <- subset( taxa_df, !is.na( Submitted_Name ) )
+taxa_na_df      <- subset( taxa_df,  is.na( Submitted_Name ) )
+taxa_na_rm_df   <- subset( taxa_df, !is.na( Submitted_Name ) )
 
 # Create function: get "cleaned" names
 get_clean_names   <- function( nam, fuzzy = 0.1 ) lcvp_search( nam, max.distance = fuzzy )
 
 # Clean names from the Leipzig's list of plants
-clean_l         <- lapply( taxa_df$Submitted_Name, get_clean_names )
+clean_l         <- lapply( taxa_na_rm_df$Submitted_Name, get_clean_names )
 clean_df        <- clean_l %>% 
   bind_rows %>% 
   rename( Submitted_Name      = Search,
@@ -70,7 +70,7 @@ mismatch_unresvd <- data.frame("Submitted_Name" = grep( 'sp.1|cf.', mismatch_df$
 clean_df_final <- data.frame("Submitted_Name" = grep( 'sp.1|cf.', clean_df$Submitted_Name, value = T, invert = T ))
 
 # Check species without matches
-no_match_v <- data.frame( "Submitted_Name" = setdiff( taxa_df$Submitted_Name, 
+no_match_v <- data.frame( "Submitted_Name" = setdiff( taxa_na_rm_df$Submitted_Name, 
                        clean_df$Submitted_Name ))
 
 # Rerun Leipzig list with fuzzy matching
@@ -88,11 +88,12 @@ taxa_out        <- lapply( clean_df_final$Submitted_Name, get_clean_names ) %>%
           LCVP_Accepted_Taxon = Output.Taxon ) %>% 
   # check for accepted names
   mutate( mismatch_test = str_detect( First_matched_Name, 
-                                      Submitted_Name ) )
+                                      Submitted_Name ), site = 'hkk' )
 
-# Do "taxa unresolved" by hand (taxa with no matches found)
-taxa_unresvd    <- bind_rows( taxa_genus_df, mismatch_unresvd, no_match_v ) %>%
-  mutate( site = 'hkk' )
+# Do "taxa unresolved" by hand (taxa with no matches found), and add back in the submitted genus, family and IDlevel to enable future identification
+taxa_unresvd    <- bind_rows( taxa_na_df, mismatch_unresvd, no_match_v ) %>%
+                    inner_join( taxa_df ) %>%
+                    mutate( site = 'hkk' ) 
   
 
 # store resolved AND unresolved taxa
