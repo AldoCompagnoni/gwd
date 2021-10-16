4902d221a6a28f69ed7addca5691272887796e8e
Ollie Spacey <ogspacey16@gmail.com>
2021-10-16 19:00:26 +0100

    Separated matched and mismatched taxa to avoid duplicates. Formatted spacing for consistency.   Checked unmatched taxa. Exported clean and unresolved taxa.

--- R/clean_format_lambir.R
+++ R/clean_format_lambir.R
@@ -85,34 +85,40 @@ clean_df <- full_join(clean_df, C_tahyanum_resvd) %>%
 # remove ... unresolved species containing "aff.", "cf.", "Sp 39", "nom. ined.", "jvl", "nom. incert.", "sp. nov.", "af." and "hairy teeth" from clean data frame
 # Archidendron casai and Castanopsis nivea are not plausible typos - move to unresolved
 # Unclear whether Kayea longipedicellata is Genus Calea or Genus Mabea - move to unresolved
-mismatch_unresvd <- data.frame("Submitted_Name" = grep(  'aff.|cf.|Sp 39|nom. ined.|jvl|nom. incert.|sp. nov.|af.|hairy teeth|Archidendron casai|Castanopsis nivea|Kayea longipedicellata', mismatch_df$Submitted_Name, value = T ))
-clean_df_final <- data.frame("Submitted_Name" = grep( 'aff.|cf.|Sp 39|nom. ined.|jvl|nom. incert.|sp. nov.|af.|hairy teeth|Archidendron casai|Castanopsis nivea|Kayea longipedicellata', clean_df$Submitted_Name, value = T, invert = T ))
-
-clean_compare <- setdiff( clean_df$Submitted_Name, clean_df_final$Submitted_Name )
+mismatch_unresvd <- data.frame( "Submitted_Name" = grep(  'aff.|cf.|Sp 39|ined.|jvl|incert.|sp. nov.|af.|hairy teeth|Archidendron casai|Castanopsis nivea|Kayea longipedicellata', mismatch_df$Submitted_Name, value = T )) 
+mismatch_unresvd <- mismatch_unresvd %>% inner_join( taxa_df ) %>% distinct()
+matched_df <- clean_df %>% subset( mismatch_test )
+clean_df_final <- data.frame( "Submitted_Name" = grep( 'aff.|cf.|Sp 39|ined.|jvl|incert.|sp. nov.|af.|hairy teeth|Archidendron casai|Castanopsis nivea|Kayea longipedicellata', mismatch_df$Submitted_Name, value = T, invert = T )) %>% full_join( matched_df )
 
 # Check species without matches; to avoid duplicates, use Sp_Code as a place holder
 add_sp_code <- select(taxa_na_rm_df, Sp_Code, Submitted_Name)
-clean_df_coded <- inner_join(clean_df, add_sp_code)
+clean_df_coded <- inner_join( clean_df, add_sp_code )
 no_match_v <- data.frame( "Sp_Code" = setdiff( taxa_na_rm_df$Sp_Code, 
                                                        clean_df_coded$Sp_Code ))
 no_match_v <- right_join( taxa_na_rm_df, no_match_v )
 
 # Rerun Leipzig list with fuzzy matching
-reclean_l       <- lapply( no_match_v, lcvp_fuzzy_search )
+reclean_l       <- lapply( no_match_v$Submitted_Name, lcvp_fuzzy_search )
 reclean_df      <- reclean_l %>% bind_rows
-# visually select species identified with lcvp_fuzzy_search
-# Only genus specified, 2 taxa remain unresolved
+# from visual inspection, no unmatched species can be resolved via fuzzy search
+# these 151 unmatched species remain unresolved
 
 # Final taxonomy files 
-taxa_nofuzzy    <- clean_df
-taxa_fuzzy      <- reclean_df
-# Combine 
-taxa_out        <- bind_rows( taxa_nofuzzy, taxa_fuzzy )
+# All clean taxa should have LCVP search results
+taxa_out        <- lapply( clean_df_final$Submitted_Name, get_clean_names ) %>% 
+  bind_rows %>% 
+  rename( Submitted_Name      = Search,
+          First_matched_Name  = Input.Taxon,
+          LCVP_Accepted_Taxon = Output.Taxon ) %>% 
+  # check for accepted names
+  mutate( mismatch_test = str_detect( First_matched_Name, 
+                                      Submitted_Name ), site = 'luquillo' )
 
-# Do "taxa unresolved" by hand (taxa with no matches found); add in 'Shorea macroptera subsp. macroptera'
-taxa_unresvd    <- bind_rows( taxa_error, )
-  data.frame( Submitted_Name = no_match_v,
-                               site           = 'lambir' )
+# Do "taxa unresolved" by hand (taxa with no matches found and unresolved mismatches); add in 'Shorea macroptera subsp. macroptera'
+
+taxa_unresvd    <- bind_rows( taxa_error, mismatch_unresvd, no_match_v ) %>%
+  inner_join( taxa_df ) %>%
+  mutate( site = 'lambir' ) 
 
 # store resolved AND unresolved taxa
 write.csv( taxa_out, 'results/lambir_taxa.csv',
