8b54a87045a41d7fed6e7d17aca14aa9d6f898d6
Ollie Spacey <ogspacey16@gmail.com>
2021-10-24 19:43:32 +0100

    Added functions to add "imputed" variable to mean and median data.

--- R/clean_format_yasuni.R
+++ R/clean_format_yasuni.R
@@ -172,3 +172,24 @@ write.csv( taxa_out, 'results/yasuni_taxa.csv',
            row.names = F )
 write.csv( taxa_unresvd, 'results/yasuni_taxa_unresvd.csv',
            row.names = F )
+
+# Prepare demographic table --------------------------------------
+
+# distiguish taxa with sample size 0 for each growth layer (1-4) and survival layer (1-4)
+yasuni_means <- yasuni_means %>% mutate( "growth_layer_1_imputed"   = grepl( "^0", growth_layer_1_obs ),
+                                               "growth_layer_2_imputed"   = grepl( "^0", growth_layer_2_obs ), 
+                                               "growth_layer_3_imputed"   = grepl( "^0", growth_layer_3_obs ), 
+                                               "growth_layer_4_imputed"   = grepl( "^0", growth_layer_4_obs ), 
+                                               "survival_layer_1_imputed" = grepl( "^0", survival_layer_1_obs ), 
+                                               "survival_layer_2_imputed" = grepl( "^0", survival_layer_2_obs ), 
+                                               "survival_layer_3_imputed" = grepl( "^0", survival_layer_3_obs ), 
+                                               "survival_layer_4_imputed" = grepl( "^0", survival_layer_4_obs ) )
+
+yasuni_medians <- yasuni_medians %>% mutate( "growth_layer_1_imputed"   = grepl( "^0", growth_layer_1_obs ),
+                                                   "growth_layer_2_imputed"   = grepl( "^0", growth_layer_2_obs ), 
+                                                   "growth_layer_3_imputed"   = grepl( "^0", growth_layer_3_obs ), 
+                                                   "growth_layer_4_imputed"   = grepl( "^0", growth_layer_4_obs ), 
+                                                   "survival_layer_1_imputed" = grepl( "^0", survival_layer_1_obs ), 
+                                                   "survival_layer_2_imputed" = grepl( "^0", survival_layer_2_obs ), 
+                                                   "survival_layer_3_imputed" = grepl( "^0", survival_layer_3_obs ), 
+                                                   "survival_layer_4_imputed" = grepl( "^0", survival_layer_4_obs ) )
